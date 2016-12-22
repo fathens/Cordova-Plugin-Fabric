7275f9e422e1f444f82e39fc2663d6294bf6a72c
Kunio Sawatani <devel@fathens.org>
2016-12-22 12:07:21 +0900

    build.gradle の編集を Gem として実装

---
+++ lib/cordova_plugin_fabric.rb
@@ -0,0 +1,10 @@
+require_relative 'cordova_plugin_fabric/gradle_file'
+
+def log(msg)
+    puts msg
+end
+
+def log_header(msg)
+    log "################################"
+    log "#### #{msg}"
+end
---
+++ lib/cordova_plugin_fabric/gradle_file.rb
@@ -0,0 +1,42 @@
+require 'pathname'
+
+def modify_gradle(target_file, api_key, api_secret)
+    File.open(target_file.dirname/'fabric.properties', 'w') { |dst|
+        dst.puts "apiKey=#{api_key}"
+        dst.puts "apiSecret=#{api_secret}"
+    }
+
+    log_header "Rewriting #{target_file}"
+    file_tmp = "#{target_file}.tmp"
+
+    srcdirs = true
+    plugin = true
+    classpath = true
+
+    File.open(target_file, 'r') { |src|
+        File.open(file_tmp, 'w') { |dst|
+            src.each_line { |line|
+                dst.puts line
+                add_line = lambda { |text|
+                    dst.puts "#{line.match(/^[\s]*/)[0]}#{text}"
+                }
+                if line =~ /^\s*mavenCentral()/
+                    add_line.call "maven { url 'https://maven.fabric.io/public' }"
+                end
+                if srcdirs && line =~ /java\.srcDirs/
+                    add_line.call "kotlin.srcDirs = ['kotlin']"
+                    srcdirs = false
+                end
+                if plugin && line =~ /apply plugin:/
+                    add_line.call "apply plugin: 'io.fabric'"
+                    plugin = false
+                end
+                if classpath && line =~ /classpath 'com\.android\.tools\.build:gradle:/
+                    add_line.call "classpath 'io.fabric.tools:gradle:1.+'"
+                    classpath = false
+                end
+            }
+        }
+    }
+    File.rename(file_tmp, target_file)
+end
