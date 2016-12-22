494ef40398ac0d250131d0563b5019e0716d3e90
Kunio Sawatani <devel@fathens.org>
2016-12-22 15:54:29 +0900

    aflter_plugin_install と before_prepare に分割
    build.gradle の編集は他のプラグインが入る前にやっておく必要があるのでこうしました。

--- platforms/android/hooks/before_prepare.rb
+++ platforms/android/hooks/before_prepare.rb
@@ -2,7 +2,6 @@
 
 require 'pathname'
 require 'rexml/document'
-require_relative '../../../lib/cordova_plugin_fabric'
 
 def search_tools(base_dir)
     Pathname.glob(base_dir/'plugins'/'*'/'plugin.xml').map { |pluginxml|
@@ -29,7 +28,9 @@ def modify_java(main_file, tools)
                     dst.puts "#{line.match(/^[\s]*/)[0]}#{text}"
                 }
                 if line =~ /super.onCreate/
-                    add_line.call "io.fabric.sdk.android.Fabric.with(this, #{instances.join(', ')});"
+                    add_line.call "try {"
+                    add_line.call "    io.fabric.sdk.android.Fabric.with(this, #{instances.join(', ')});"
+                    add_line.call "} catch (Exception ex) { throw new RuntimeException(ex); }"
                 end
             }
         }
@@ -39,7 +40,5 @@ end
 
 $PROJECT_DIR = Pathname.pwd.realpath
 $PLATFORM_DIR = $PROJECT_DIR/'platforms'/'android'
-
-modify_gradle $PLATFORM_DIR/'build.gradle', ENV['FABRIC_API_KEY'], ENV['FABRIC_BUILD_SECRET']
 
 modify_java Pathname.glob($PLATFORM_DIR/'src'/'**'/'MainActivity.java').first, search_tools($PROJECT_DIR)
