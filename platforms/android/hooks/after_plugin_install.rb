abe5b80283a257c3ee65d6652a240fa572ec435c
Kunio Sawatani <devel@fathens.org>
2016-12-26 17:46:59 +0900

    ライブラリを使って Android の hooks を簡素に

--- platforms/android/hooks/after_plugin_install.rb
+++ platforms/android/hooks/after_plugin_install.rb
@@ -6,4 +6,4 @@ require_relative '../../../lib/cordova_plugin_fabric'
 $PROJECT_DIR = Pathname.pwd.realpath
 $PLATFORM_DIR = $PROJECT_DIR/'platforms'/'android'
 
-modify_gradle $PLATFORM_DIR/'build.gradle', ENV['FABRIC_API_KEY'], ENV['FABRIC_BUILD_SECRET']
+Fabric::modify_gradle $PLATFORM_DIR/'build.gradle', ENV['FABRIC_API_KEY'], ENV['FABRIC_BUILD_SECRET']
--- platforms/android/hooks/before_prepare.rb
+++ platforms/android/hooks/before_prepare.rb
@@ -1,32 +1,13 @@
 #!/usr/bin/env ruby
 
 require 'pathname'
-require 'cordova_plugin_fabric'
-
-def modify_java(main_file, kits)
-    return if main_file.nil? || tools.empty?
-
-    file_tmp = "#{main_file}.tmp"
-
-    File.open(main_file, 'r') { |src|
-        File.open(file_tmp, 'w') { |dst|
-            src.each_line { |line|
-                dst.puts line
-                add_line = lambda { |text|
-                    dst.puts "#{line.match(/^[\s]*/)[0]}#{text}"
-                }
-                if line =~ /super.onCreate/
-                    add_line.call "try {"
-                    add_line.call "    io.fabric.sdk.android.Fabric.with(this, #{kits.instances.join(', ')});"
-                    add_line.call "} catch (Exception ex) { throw new RuntimeException(ex); }"
-                end
-            }
-        }
-    }
-    File.rename(file_tmp, main_file)
-end
+require_relative '../../../lib/cordova_plugin_fabric'
 
 $PROJECT_DIR = Pathname.pwd.realpath
 $PLATFORM_DIR = $PROJECT_DIR/'platforms'/'android'
 
-modify_java Pathname.glob($PLATFORM_DIR/'src'/'**'/'MainActivity.java').first, Fabric::Kits.new('android', $PROJECT_DIR)
+kits = Fabric::Kits.new('android', $PROJECT_DIR)
+Fabric::modify_line $PLATFORM_DIR/'kotlin'/'org.fathens.cordova.plugin.fabric'/'FabricBase.kt', {
+    /^\/\/\s*import\s*Kits$/ => kits.imports,
+    /^\s*\/\/\s*Kits\s*$/ => kits.instances.join(', ')
+}
